9d2b199bc27186ef2a407091ebe00b6009db6d04
Hardik Dhamija <hardikdhamija96@gmail.com>
2024-07-11 22:22:33 +0530

    Navbar Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-    <h1 className="text-center text-2xl py-5">Hello World!</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-center text-2xl py-5">Hello World!</h1>
+    </main>
   );
 }
